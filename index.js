becbde3c3fd482f85ca3e5122d6f4344cba61299
Dominic <Dominic.decelle@gmail.com>
2023-09-01 11:24:35 -0700

    Completed Arithmetic Prework Lab

---
+++ index.js
@@ -0,0 +1,53 @@
+function add() {
+    // will do shortly
+}
+
+function subtract() {
+    //w will do shortly
+}
+
+function multiply() {
+    // will do shortly
+}
+
+function divide() {
+  // will do shortly   
+}
+
+function add(a, b) {
+    return a + b; // will figure this out
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+
+let a = 2
+let b = 4
+
+function increment(a) {
+    return a + 1
+}
+
+
+function decrement(b) {
+    return b - 1
+}
+
+function makeInt(a) {
+    return parseInt( a, 10);
+}
+
+function preserveDecimal(a) {
+    return parseFloat(a, 10)
+}
